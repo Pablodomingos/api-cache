b26972087ff7670ffe003720297a32b11f5f6242
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-23 18:21:16 -0300

    Refactor: added observers.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,19 @@
 
 namespace App\Providers;
 
+use App\Models\{
+    Course,
+    Lesson,
+    Module
+};
+use App\Observers\{
+    CourseObserver,
+    LessonObserver,
+    ModuleObserver
+};
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +34,9 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Course::observe(CourseObserver::class);
+        Module::observe(ModuleObserver::class);
+        Lesson::observe(LessonObserver::class);
     }
 
     /**
