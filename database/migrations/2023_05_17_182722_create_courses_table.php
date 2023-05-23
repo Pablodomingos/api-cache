90843b82cb3e997a3a51a2f120234fad6a193705
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-23 18:12:50 -0300

    Refactor: adjusted columns.

--- database/migrations/2023_05_17_182722_create_courses_table.php
+++ database/migrations/2023_05_17_182722_create_courses_table.php
@@ -10,9 +10,9 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid');
+            $table->uuid();
             $table->string('name', 255);
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
