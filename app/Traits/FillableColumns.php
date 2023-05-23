8033019bbb93bc6069b2228ca27fbd4167a19ad0
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-23 18:22:14 -0300

    Refactor: function tweaked to improve queries.

--- app/Traits/FillableColumns.php
+++ app/Traits/FillableColumns.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 trait FillableColumns
 {
-    public function getFillable()
+    public function getFillableColumns()
     {
-        return Schema::getColumnListing($this->getTable());
+        $attributes = Schema::getColumnListing($this->getTable());
+        return array_values(array_filter($attributes, fn ($value) => !in_array($value, $this->getGuarded()) || in_array($value, $this->getFillable())));
     }
 }
