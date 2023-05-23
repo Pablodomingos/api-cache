581928c708606b4a255374a73742477f172cd85f
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-23 18:16:25 -0300

    Feat: created file for padronize return of the values.

---
+++ app/Http/Resources/CourseResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->uuid,
+            'title' => $this->name,
+            'description' => $this->description,
+            'date' => Carbon::make($this->created_at)->format('Y-m-d'),
+            'modules' => ModuleResource::collection($this->whenLoaded('modules'))
+        ];
+    }
+}
