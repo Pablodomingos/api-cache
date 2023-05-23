9b43af7c41c21f7deff6e4d2e2f4f6a043c8f566
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-23 18:19:52 -0300

    Refactor: adjusted file.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,15 +5,19 @@ namespace App\Models;
 use App\Traits\FillableColumns;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * App\Models\Course
  *
  * @property int $id
+ * @property string $uuid
  * @property string $name
- * @property string $description
+ * @property string|null $description
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Module> $modules
+ * @property-read int|null $modules_count
  * @method static \Database\Factories\CourseFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|Course newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Course newQuery()
@@ -23,9 +27,21 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Course whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Course whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Course whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereUuid($value)
  * @mixin \Eloquent
  */
 class Course extends Model
 {
     use HasFactory, FillableColumns;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function modules(): HasMany
+    {
+        return $this->hasMany(Module::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -36,13 +36,17 @@ use Laravel\Sanctum\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
  * @mixin \Eloquent
  */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, FillableColumns;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
 
     protected $hidden = [
         'password',
