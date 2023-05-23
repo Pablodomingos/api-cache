e1622318f8230926f02d7a65c369cfa0967c9c7f
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-23 18:00:46 -0300

    Feat: added structure for the functions

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUpdateCourseRequest;
+use App\Http\Resources\CourseResource;
 use App\Repositories\Interfaces\CourseRepositoryInterface;
 use App\Services\CourseService;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CourseController extends Controller
 {
@@ -18,28 +20,37 @@ class CourseController extends Controller
 
     public function index()
     {
-        return response()->json(
+        return CourseResource::collection(
             $this->courseRepository->all()
         );
     }
 
-    public function store(Request $request)
+    public function store(CreateUpdateCourseRequest $request)
     {
-        //
+        return new CourseResource(
+            $this->courseRepository->create($request->validated())
+        );
     }
 
-    public function show(int $id)
+    public function show(string $uuid)
     {
-        //
+        return new CourseResource(
+            $this->courseRepository->findByUuid($uuid)
+        );
     }
 
-    public function update(Request $request, int $id)
+    public function update(CreateUpdateCourseRequest $request, string $uuid)
     {
-        //
+        return new CourseResource(
+            $this->courseRepository->updateByUuid($request->validated(), $uuid)
+        );
     }
 
-    public function destroy(int $id)
+    public function destroy(string $uuid)
     {
-        //
+        return response()->json(
+            $this->courseRepository->deleteByUuid($uuid),
+            Response::HTTP_NO_CONTENT
+        );
     }
 }
